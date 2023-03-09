3283d25c3e1ce24e690b4d0717b68a4caa30cfab
Monty Rasmussen <montyr75@gmail.com>
2023-03-08 22:38:48 -0700

    ssl

--- lib/db/appwrite.dart
+++ lib/db/appwrite.dart
@@ -4,7 +4,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'appwrite.g.dart';
 
 const projectID = '640509d1427ecb8ca360';
-const endpoint = 'http://137.184.84.239/v1';
+const endpoint = 'https://137.184.84.239/v1';
 const dbID = '64050affc996c7c754bf';
 
 @riverpod
